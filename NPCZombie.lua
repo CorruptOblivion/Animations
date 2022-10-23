3204b73f81383eb1711923d37e475123915f9349
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-10-23 10:50:09 -0700

    Create NPCZombie.lua

---
+++ NPCZombie.lua
@@ -0,0 +1,9 @@
+
+    local Animate = game.Players.LocalPlayer.Character.Animate
+    Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=3489171152"
+    Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=3489171152"
+    Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=3489174223"
+    Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=3489173414"
+    Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=616161997"
+    Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=616156119"
+    Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=616157476"
